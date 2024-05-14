4997a2654e9e5ee1b28f044ea50be8cfdb676280
Айдин Нурланбеков <nurlanbekovich.a@gmail.com>
2024-05-14 23:43:05 +0600

    _

---
+++ StudentPortal.Web/Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Data;
+using StudentPortal.Web.Models;
+using StudentPortal.Web.Models.Entities;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public StudentController(ApplicationDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Student()
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subcriped = viewModel.Subcriped,
+            };
+            await _dbContext.Students.AddAsync(student);
+            await _dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> List() 
+        {
+            var students=await _dbContext.Students.ToListAsync();
+        }
+    }
+}
